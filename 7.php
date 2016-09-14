af70d9fc1cc1ca4dcfe9ef86f82edd3a8f420b85
Igor R <bocharova135@gmail.com>
2016-09-14 17:16:17 +0300

    add 7-8.php

---
+++ 7.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IgorPc
+ * Date: 14.09.2016
+ * Time: 16:48
+ */
+
+$arr = array(2,5,9,15,0,4);
+
+foreach ($arr as $item) {
+    if ( $item > 3 && $item < 10){
+        echo $item . "<br>";
+
+    }
+
+}
---
+++ 8.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IgorPc
+ * Date: 14.09.2016
+ * Time: 17:11
+ */
+
+$arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+$str = "";
