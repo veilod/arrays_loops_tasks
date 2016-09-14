db6f4ace843f12877c86810821e0acf38f819bb6
Igor R <bocharova135@gmail.com>
2016-09-14 16:48:34 +0300

    6.php completed

--- 6.php
+++ 6.php
@@ -9,6 +9,20 @@
 $arr = array('green'=>'зеленый', 'red'=>'красный','blue'=>'голубой');
 
 foreach ($arr as $key => $item) {
+    $en[] = $key;
+
+    $ru[] = $item;
+
+}
+
+foreach ($en as $value) {
+    echo $value . "<br>";
+}
 
 
+echo "<br>";
+echo "<br>";
+
+foreach ($ru as $rus) {
+    echo $rus . "<br>";
 }
